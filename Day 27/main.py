03712410702ca32b1d1e6fea4601654f916ef495
tanishqmudaliar <tanishqmudaliar846@gmail.com>
2026-08-10 21:31:58 +0530

    Add GUI button functionality and display car details in playground

--- Day 27/main.py
+++ Day 27/main.py
@@ -7,4 +7,14 @@ window.minsize(500, 300)
 my_label = tkinter.Label(text="I am a label", font=("Times New Roman", 24, "bold"))
 my_label.pack()
 
+def button_clicked():
+    answer = input.get() if input.get() != "" else "No Input"
+    my_label["text"] = answer
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+input = tkinter.Entry(width=20)
+input.pack()
+
 window.mainloop()
--- Day 27/playground.py
+++ Day 27/playground.py
@@ -17,3 +17,13 @@ def calculator(n, **kwargs):
     return n
 
 print(calculator(2, add=3, multiply=5))
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car)
+print(my_car.make)
+print(my_car.model)
